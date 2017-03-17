b4ad9acb67d7061d6c5e7349606f99e4e5c2d783
liuyzh <liuyzh@tju.edu.cn>
2017-03-17 22:18:39 +0800

    cpp

--- cpp_primer/3/3.44.cpp
+++ cpp_primer/3/3.44.cpp
@@ -16,15 +16,15 @@ using std::string;
 int main(void) {
     int ia[3][4] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     using int_array = int[4];
-    cout << "range for" << endl;
+    cout << "PART1" << endl;
     for (int_array &p : ia)
         for (int i : p)
             cout << i << endl;
-    cout << "size_t" << endl;
+    cout << "PART2" << endl;
     for (size_t i = 0; i < 3; i++)
         for (size_t j = 0; j < 4; j++)
             cout << ia[i][j] << endl;
-    cout << "*p" << endl;
+    cout << "PART3" << endl;
     for (int_array *row = begin(ia); row != end(ia); row++)
         for (int *col = begin(*row); col != end(*row); col++)
             cout << *col << endl;
--- cpp_primer/3/3.45.cpp
+++ cpp_primer/3/3.45.cpp
@@ -16,16 +16,16 @@ using std::string;
 int main(void) {
     int ia[3][4] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     cout << "PART1" << endl;
-    for (const int (&row)[4] : ia)
-        for (int i : row)
-            cout << i << endl;
+    for (auto &row : ia)
+        for (auto col : row)
+            cout << col << endl;
     cout << "PART2" << endl;
     for (size_t i = 0; i < 3; i++)
         for (size_t j = 0; j < 4; j++)
             cout << ia[i][j] << endl;
     cout << "PART3" << endl;
-    for (int (*row)[4] = begin(ia); row != end(ia); row++)
-        for (int *col = begin(*row); col != end(*row); col++)
-            cout << *col << endl;
+    for (auto p = begin(ia); p != end(ia); p++)
+        for (auto q = begin(*p); q != end(*p); q++)
+            cout << *q << endl;
     return 0;
 }
