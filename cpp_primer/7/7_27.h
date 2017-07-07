a0dd5e631c26ff5e61d2dc123e82dc2268ba0bb0
liuyzh <liuyzh@tju.edu.cn>
2017-07-07 16:17:43 +0800

    cpp

--- cpp_primer/7/7_27.h
+++ cpp_primer/7/7_27.h
@@ -1,5 +1,5 @@
-#ifndef cpp_7_24_h
-#define cpp_7_24_h
+#ifndef cpp_7_27_h
+#define cpp_7_27_h
 
 #include <iostream>
 #include <string>
@@ -16,10 +16,23 @@ public:
     }
     inline char get(pos ht, pos wd) const;
     Screen &move(pos r, pos c);
+    Screen &set(char);
+    Screen &set(pos, pos, char);
+    Screen &display(std::ostream &os) {
+        do_display(os);
+        return *this;
+    }
+    const Screen &display(std::ostream &os) const {
+        do_display(os);
+        return *this;
+    }
 private:
     pos cursor = 0;
     pos height = 0, width = 0;
     std::string contents;
+    void do_diaplay(std::ostream &os) const {
+        os << contents;
+    }
 };
 
 inline Screen &Screen::move(pos r, pos c) {
@@ -33,8 +46,18 @@ char Screen::get(pos r, pos c) const {
     return contents[row + c];
 }
 
+inline Screen &Screen::set(char c) {
+    contents[cursor] = c;
+    return *this;
+}
+
+inline Screen &Screen::set(pos r, pos col, char ch) {
+    contents[r * width + col] = ch;
+    return *this;
+}
+
 class Windows_mgr {
-private:
+    private:
     std::vector<Screen> screens{Screen(24, 80, ' ')};
 };
 
