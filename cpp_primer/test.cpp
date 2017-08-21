6117e33480bd81d0656f6f96edb982634083f9b2
liuyzh <liuyzh@tju.edu.cn>
2017-08-21 16:39:34 +0800

    test: struct

--- cpp_primer/test.cpp
+++ cpp_primer/test.cpp
@@ -1,21 +1,29 @@
 #include<iostream>
-#include<forward_list>
+#include<string>
 
 using std::cin; using std::cout; using std::endl;
-using std::forward_list;
+using std::string;
 
-int main() {
-    forward_list<int> lst(5, 5);
+struct Sales_data {
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+    //成员函数
+    void combine(const Sales_data&);
+};
 
-    for (auto i : lst) cout << i << ' ';
-    cout << endl;
+void Sales_data::combine(const Sales_data &rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+}
 
-    auto it = lst.insert_after(lst.before_begin(), {1, 2, 3, 4});
-
-    for (auto i : lst) cout << i << ' ';
-    cout << endl;
-
-    cout << *it;
-
+int main () {
+    Sales_data total;
+    total.units_sold = 0;
+    total.revenue = 0;
+    Sales_data trans;
+    trans.units_sold = 1;
+    trans.revenue = 1.0;
+    total.combine(trans);
+    cout << total.units_sold << ' ' << total.revenue << endl;
     return 0;
 }
