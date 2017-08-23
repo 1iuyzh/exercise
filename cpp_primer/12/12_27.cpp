58f5eadf382d02433eedd044369b3625685eca61
liuyzh <liuyzh@tju.edu.cn>
2017-08-23 20:45:12 +0800

    12_27.cpp added

---
+++ cpp_primer/12/12_27.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<memory>
+#include<string>
+
+using std::cin; using std::cout; using std::endl;
+using std::allocator;
+using std::string;
+
+int main() {
+    int n = 10;
+    allocator<string> alloc;
+    auto const p = alloc.allocate(n);
+    auto q = p;
+    string s;
+    while (cin >> s && q != p + n) {
+        alloc.construct(q++, s);
+    }
+    
+    while (q != p) {
+        cout << *--q << ' ';
+        alloc.destroy(q);
+    }
+    alloc.deallocate(p, n);
+    return 0;
+}
