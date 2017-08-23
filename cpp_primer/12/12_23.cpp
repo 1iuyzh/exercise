3ecb1224ecd7c92fc766380b01613171787ef9ec
liuyzh <liuyzh@tju.edu.cn>
2017-08-23 17:28:34 +0800

    12_23.cpp modified

--- cpp_primer/12/12_23.cpp
+++ cpp_primer/12/12_23.cpp
@@ -2,8 +2,10 @@
 // 或者调用<string.h>
 // strlen()返回C风格字符串数组的大小, 不包括'\0'
 #include<cstring>
+#include<string>
 
 using std::cin; using std::cout; using std::endl;
+using std::string;
 
 int main() {
     const char *s1 = "Alpha";
@@ -15,5 +17,10 @@ int main() {
     strcat(s, s2);
     cout << s << endl;
     delete [] s;
+
+    // part 2
+    string str1("Beta");
+    string str2("Gamma");
+    cout << str1 + str2 << endl;
     return 0;
 }
