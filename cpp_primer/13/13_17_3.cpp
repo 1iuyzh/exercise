28587bbc5743f1b1f1eeefb08e6b2f8b3d252e4a
liuyzh <liuyzh@tju.edu.cn>
2017-08-25 09:09:53 +0800

    13_17 added

---
+++ cpp_primer/13/13_17_3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using std::cin; using std::cout; using std::endl;
+
+// 默认构造函数为每个对象生成一个唯一的序号
+// 自己定义拷贝构造函数
+class numbered {
+public:
+    friend void f(const numbered &s);
+    numbered() : msyn(unique++) { }
+    numbered(const numbered &n) : msyn(unique++) { }
+    static int unique;
+    int msyn;
+};
+
+void f(const numbered &s) {
+    cout << s.msyn << endl;
+}
+
+// 在所有函数之外定义并初始化静态成员
+int numbered::unique = 0;
+
+int main() {
+    numbered a, b = a, c = b;
+    f(a); f(b); f(c);
+    return 0;
+}
