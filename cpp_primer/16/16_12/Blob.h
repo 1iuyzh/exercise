cd8168d07433d16c3ddf2593aee0d0a5d7b80eab
liuyzh <liuyzh@tju.edu.cn>
2017-09-22 22:29:56 +0800

    updated

---
+++ cpp_primer/16/16_12/Blob.h
@@ -0,0 +1,82 @@
+#ifndef BLOB_H
+#define BLOB_H
+
+#include"BlobPtr.h"
+#include<memory>
+#include<vector>
+#include<string>
+
+template <typename> class BlobPtr;
+
+template <typename T> class Blob {
+    friend class BlobPtr<T>;
+
+public:
+    typedef T value_type;
+    typedef typename std::vector<T>::size_type size_type;
+    // 构造函数
+    Blob();
+    Blob(std::initializer_list<T> il);
+    // Blob中的元素数目
+    size_type size() const { return data->size(); }
+    bool empty() const { return data->empty(); }
+    // 添加和删除元素
+    void push_back(const T& t) { data->push_back(t); }
+    // 移动版本
+    void push_back(T&& t) { data->push_back(std::move(t)); }
+    void pop_back();
+    // 元素访问
+    T& back();
+    T& operator[](size_type i);
+    const T& back() const;
+    const T& operator[](size_type i) const;
+private:
+    std::shared_ptr<std::vector<T>> data;
+    // 若data[i]无效, 则抛出msg
+    void check(size_type i, const std::string &msg) const;
+};
+
+template <typename T>
+Blob<T>::Blob() : data(std::make_shared<std::vector<T>>()) { }
+
+template <typename T>
+Blob<T>::Blob(std::initializer_list<T> il) : 
+    data(std::make_shared<std::vector<T>>(il)) { }
+
+template <typename T>
+void Blob<T>::check(size_type i, const std::string &msg) const {
+    if (i >= data->size())
+        throw std::out_of_range(msg);
+}
+
+template <typename T>
+T& Blob<T>::back() {
+    check(0, "back on empty Blob");
+    return data->back();
+}
+
+template <typename T>
+const T& Blob<T>::back() const {
+    check(0, "back on empty Blob");
+    return data->back();
+}
+
+template <typename T>
+T& Blob<T>::operator[](size_type i) {
+    check(i, "subscript out of range");
+    return (*data)[i];
+}
+
+template <typename T>
+const T& Blob<T>::operator[](size_type i) const {
+    check(i, "subscript out of range");
+    return (*data)[i];
+}
+
+template <typename T>
+void Blob<T>::pop_back() {
+    check(0, "pop_bacl on empty Blob");
+    data->pop_back();
+}
+
+#endif
---
+++ cpp_primer/16/16_12/BlobPtr.h
@@ -0,0 +1,89 @@
+#ifndef BLOBPTR_H
+#define BLOBPTR_H
+
+#include"Blob.h"
+#include<memory>
+#include<vector>
+#include<string>
+
+template <typename T> class BlobPtr;
+template <typename T> class Blob;
+
+template <typename T>
+bool operator==(const BlobPtr<T> &lhs, const BlobPtr<T> &rhs);
+
+template <typename T>
+bool operator<(const BlobPtr<T> &lhs, const BlobPtr<T> &rhs);
+
+template <typename T> class BlobPtr {
+    friend bool operator==<T> (const BlobPtr<T> &, const BlobPtr<T> &);
+    friend bool operator< <T> (const BlobPtr<T> &, const BlobPtr<T> &);
+
+public:
+    BlobPtr() : curr(0) { }
+    BlobPtr(Blob<T> &a, size_t sz = 0) : 
+        wptr(a.data), curr(sz) { }
+    T&  operator*() const {
+        auto p = check(curr, "dereference past end");
+        return (*p)[curr];
+    }
+    BlobPtr& operator++();
+    BlobPtr& operator--();
+    BlobPtr operator++(int);
+    BlobPtr operator--(int);
+private:
+    std::shared_ptr<std::vector<T>> check(std::size_t, const std::string&) const {
+        if (this->wptr.lock()) {
+            auto ret = this->wptr.lock();
+            return ret;
+        }
+    }
+    std::weak_ptr<std::vector<T>> wptr;
+    std::size_t curr;   // 数组中的当前位置
+};
+
+// 前置
+template <typename T>
+BlobPtr<T>& BlobPtr<T>::operator++() {
+    check(curr, "increment past end of Blob");
+    ++curr;
+    return *this;
+}
+
+template <typename T>
+BlobPtr<T>& BlobPtr<T>::operator--() {
+    --curr;
+    check(curr, "decrement past begin of Blob");
+    return *this;
+}
+
+// 后置
+template <typename T>
+BlobPtr<T> BlobPtr<T>::operator++(int) {
+    BlobPtr ret = *this;
+    ++*this;
+    return ret;
+}
+
+template <typename T>
+BlobPtr<T> BlobPtr<T>::operator--(int) {
+    BlobPtr ret = *this;
+    --*this;
+    return ret;
+}
+
+template <typename T>
+bool operator==(const BlobPtr<T> &lhs, const BlobPtr<T> &rhs) {
+    //if (lhs.wptr.lock() != rhs.wptr.lock())
+        //throw runtime_error("ptrs to different Blobs!");
+    return lhs.curr == rhs.curr;
+}
+
+template <typename T>
+bool operator<(const BlobPtr<T> &lhs, const BlobPtr<T> &rhs) {
+    //if (lhs.wptr.lock() != rhs.wptr.lock())
+        //throw runtime_error("ptrs to different Blobs!");
+    return lhs.curr < rhs.curr;
+}
+
+#endif
