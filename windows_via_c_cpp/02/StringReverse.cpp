12dfea90bce3d290830f5297628f62a2faa12fd2
liuyzh <liuyzh@tju.edu.cn>
2018-01-25 22:53:42 +0800

    cpp

--- windows_via_c_cpp/02/StringReverse.cpp
+++ windows_via_c_cpp/02/StringReverse.cpp
@@ -1,15 +1,16 @@
-#define _UNICODE
-#define UNICODE
+//#define _UNICODE
+//#define UNICODE
 
 #include"StringReverse.h"
 #include<iostream>
 #include<windows.h>
 using std::wcout;
+using std::endl;
 
 int main() {
-    WCHAR mystr[] = L"unicode";
-    PWSTR mypt = mystr;
-    if (MyStringReverse(mypt))
-        wcout << mypt;
+    TCHAR str[] = _T("abcdef");
+    PTSTR pt = str;
+    if (MyStringReverse(pt))
+        wcout << pt << endl;
     return 0;
 }
--- windows_via_c_cpp/02/StringReverse.h
+++ windows_via_c_cpp/02/StringReverse.h
@@ -1,14 +1,14 @@
 #ifndef STRINGRESERVE
 #define STRINGRESERVE
 
+#include<tchar.h>
+#include<windows.h>
+
 #ifdef UNICODE
 #define MyStringReverse MyStringReserveW
 #else
 #define MyStringReverse MyStringReserveA
 #endif
-
-#include<tchar.h>
-#include<windows.h>
 
 BOOL MyStringReserveW(PWSTR pWideCharStr) {
     // Get a pointer to the last char in the string.
@@ -39,7 +39,7 @@ BOOL MyStringReserveA(PSTR pMultiByteStr) {
     nLenOfWideCharStr = MultiByteToWideChar(CP_ACP, 0, pMultiByteStr, -1, NULL, 0);
     // Alloc memory from the process's default heap to accommodate the size of the wide-char string.
     // Don't forget that MultiByteToWideChar returns the number of char, not the numbers of bytes, so you must multiply by the size of a wide char.
-    pWideCharStr = HeapAlloc(GetProcessHeap(), 0, nLenOfWideCharStr*sizeof(WCHAR));
+    pWideCharStr = reinterpret_cast<PWSTR> (HeapAlloc(GetProcessHeap(), 0, nLenOfWideCharStr*sizeof(WCHAR)));
     if (pWideCharStr == nullptr)
         return (fOk);
     // Convert the multibyte string to a wide-char string.
