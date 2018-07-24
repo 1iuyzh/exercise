cad2d8a02f09fa63571523b051a3ae4e81e237d0
liuyzh <liuyzh@tju.edu.cn>
2018-07-24 23:12:37 +0800

    cpp

---
+++ cpp_concurrency/01/hello_world.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<thread>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::thread;
+
+void hello() {
+    cout << "Hello Concurrent World" << endl;
+}
+
+int main() {
+    thread t(hello);
+    t.join();
+    return 0;
+}
