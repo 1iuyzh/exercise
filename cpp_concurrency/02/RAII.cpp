ffbd8ad1a23b700906a48294b628cbb29a0852ce
liuyzh <liuyzh@tju.edu.cn>
2018-07-25 12:09:13 +0800

    cpp

---
+++ cpp_concurrency/02/RAII.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<thread>
+
+class thread_guard {
+    std::thread &t;
+public:
+    explicit thread_guard(std::thread &t_) : t(t_) {}
+    ~thread_guard() {
+        if (t.joinable())
+            t.join();
+    }
+    thread_guard(const thread_guard&) = delete;
+    thread_guard& operator=(const thread_guard&) = delete;
+};
+
+struct func {
+    int &i;
+    func(int &i_) : i(i_) {}
+    void operator() () {
+        for (unsigned j = 0; j < 10; ++j)
+            std::cout << i;
+    }
+};
+
+void f() {
+    int some_local_state = 1;
+    func my_func(some_local_state);
+    std::thread t(my_func);
+    thread_guard g(t);
+    for (unsigned j = 0; j < 10; ++j)
+        std::cout << 0;
+}
+
+int main() {
+    f();
+    return 0;
+}
